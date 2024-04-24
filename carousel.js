99a92b32fd5bdfaddadd5859cc8157471f9755f3
rilesSmiles <riley.schmitz@edu.sait.ca>
2024-04-23 23:58:56 -0600

    carousel styled

--- carousel.js
+++ carousel.js
@@ -1,5 +1,5 @@
 let currentSlide = 0;
-const slides = document.querySelectorAll('.carousel-slide');
+const slides = document.querySelectorAll('.carousel-pic');
 
 function showSlide(index) {
     slides.forEach((slide, i) => {
