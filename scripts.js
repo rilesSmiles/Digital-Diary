90bb52394a4d1cb6db5870a5c1401ec9b04c13cf
rilesSmiles <riley.schmitz@edu.sait.ca>
2024-04-23 21:30:04 -0600

    hero banner fixed

---
+++ scripts.js
@@ -0,0 +1,21 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+
+setInterval(nextSlide, 1000);
